cb1e3f4a6f65b15f5b8a38571bb9799325071d52
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2013-12-11 00:32:48 +0100

    change on the controllers and the mail sent

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -39,8 +39,6 @@ class HomeController extends BaseController {
 
 		if ($validator->passes()) {
 			// We have to send the e-mail(s).
-			// Following this tutorial to send the mail:
-			// http://net.tutsplus.com/tutorials/html-css-techniques/build-a-neat-html5-powered-contact-form/
 			$name = Input::get('name');
 			$email = Input::get('email');
 			$message = Input::get('message');
@@ -48,26 +46,14 @@ class HomeController extends BaseController {
 			$ipaddress = $_SERVER['REMOTE_ADDR'];  
 	    $date = date('d/m/Y');  
 	    $time = date('H:i:s'); 
-
-			$headers = "From: " . $email . "<script type=\"text/javascript\"> 
-			/* <![CDATA[ */ 
-			(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(\"script\");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); 
-			/* ]]> */ 
-			</script>" . "\r\n";  
 			
-			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";  
-			      
 			$emailbody = "<p>You have recieved a new message from the enquiries form on your website.</p> 
 	                  <p><strong>Name: </strong> {$name} </p> 
 	                  <p><strong>Email Address: </strong> {$email} </p> 
 	                  <p><strong>Message: </strong> {$message} </p> 
 	                  <p>This message was sent from the IP Address: {$ipaddress} on {$date} at {$time}</p>";  
 			      
-			mail("st.zavrakas@gmail.com<script type=\"text/javascript\"> 
-			/* <![CDATA[ */ 
-			(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(\"script\");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); 
-			/* ]]> */ 
-			</script>","New Enquiry", $emailbody, $headers);
+			mail(to, subject, message)("st.zavrakas@gmail.com", "New Enquiry", $emailbody);
 
 			return 'Data was sent.';
 		}
